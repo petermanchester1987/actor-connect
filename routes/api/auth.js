6e0d00b33aa2051840db948898389df34fa38575
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-09-22 19:40:58 +0100

    added middleware auth route with jwt

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,23 @@
 const express = require("express");
-
 const router = express.Router();
+const auth = require("../../middleware/auth");
+
+//have to bring in User model
+const User = require("../../models/User");
 
 // @route   GET api/auth
 // @desc    Test route
-// @access  Public
-router.get("/", (req, res) => res.send("auth route"));
+// @access  Private
+router.get("/", auth, async (req, res) => {
+  try {
+    //so you're making a request to the database and then finding by Id, using
+    //the previously auth user id, but not sending the password back!!
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
