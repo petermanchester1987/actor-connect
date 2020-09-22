93b49ce719d4875f1ed76643504e4e2f635a56ef
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-09-22 17:42:36 +0100

    made different files for different api routes

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
+//initialize express
 const app = express();
+
+//connect database
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API running");
