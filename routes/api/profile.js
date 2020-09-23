56100273936a222162a24af3255cae3886124068
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-09-23 18:07:37 +0100

    added route for if i were to use an api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator");
+const request = require("request");
+const config = require("config");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
@@ -369,5 +371,61 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+//
+//
+//
+// //
+// // @route   GET api/profile/spotlight/:username
+// // @desc    get user spotlight link
+// // @access  Public
+// //
+// router.get("/spotlight/:username", (req, res) => {
+//   try {
+//     /* IF WE WERE GETTING A REAL API
+
+//       IT would be
+
+//       const options = {
+//       uri: `https://api.github.com/
+//       users/${req.params.username}/
+//       repos?
+//       per_page=5&
+//       sort=created:asc&
+//       client_id=${config.get('githubClientId)}&
+//       client_secret=${config.get('githubSecret)}`,
+//       method: 'GET',
+
+//       sometime this may be needed..check docs
+//       headers: { 'user-agent': 'node.js' }
+//         };
+
+//         request(options, (error, response, body) => {
+//           if(error) console.error(error);
+
+//           if(response.statusCode !== 200) {
+//             res.status(404).json({ msg: 'No Spotlight Profile Found'})
+//           }
+//       })
+
+//       */
+//     const options = {
+//       uri: `https://www.spotlight.com/${req.params.username}`,
+//     };
+
+//     request(options, (error, response, body) => {
+//       if (error) console.error(error);
+
+//       if (response.statusCode !== 200) {
+//         res.status(404).json({ msg: "No Spotlight Profile Found" });
+//       }
+
+//       res.json(JSON.parse(body));
+//     });
+//   } catch (err) {
+//     console.error(err.message);
+//     res.status(500).send("Server Error");
+//   }
+// });
 
 module.exports = router;
