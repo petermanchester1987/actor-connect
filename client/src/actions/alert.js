675f9ae36ad24d132bf52de3aa53571e9c146bbe
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-09-24 15:07:51 +0100

    added action into resister auth component

---
+++ client/src/actions/alert.js
@@ -0,0 +1,13 @@
+import uuid from "uuid";
+
+import { SET_ALERT, REMOVE_ALERT } from "./types";
+
+//thunk middleware allows you to send OTHER actiontypes
+//using the dispatch
+export const setAlert = (msg, alertType) => (dispatch) => {
+  const id = uuid.v4();
+  dispatch({
+    type: SET_ALERT,
+    payload: { msg, alertType, id },
+  });
+};
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,9 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
 
-const Register = () => {
+const Register = (props) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -23,7 +25,7 @@ const Register = () => {
   const onSubmit = (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("Passwords do not match");
+      props.setAlert("Passwords do not match", "danger");
     } else {
       console.log("Success");
     }
@@ -96,4 +98,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default connect(null, { setAlert })(Register);
