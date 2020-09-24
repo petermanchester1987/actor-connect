7b097ed640a5aed3496aea47d1b51412f0a50b9d
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-09-24 18:16:18 +0100

    added PrivateRoute component

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
 
 // Redux stuff
 import { Provider } from "react-redux";
