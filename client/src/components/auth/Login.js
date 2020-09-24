27c6fb068adfe1a954e66a7a8ec9e1e071f435b8
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-09-24 14:22:27 +0100

    added login and register components

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  //destructuring so you dont have to write formData.name
+  const { email, password } = formData;
+
+  //making onChange function
+  //the e.target.name is so that the key is dependant on the
+  //target being changed
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  //onSubmit function
+  //stop default form action
+  //will change the error handling later
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Success");
+  };
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i>Sign Into Your Account
+      </p>
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={(e) => onChange(e)}
+            required
+            minLength="6"
+          />
+        </div>
+
+        <input
+          name="submit"
+          type="submit"
+          value="Login"
+          className="btn btn-primary"
+        />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </Fragment>
+  );
 };
 
 export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -24,7 +25,7 @@ const Register = () => {
     if (password !== password2) {
       console.log("Passwords do not match");
     } else {
-      console.log(formData);
+      console.log("Success");
     }
   };
 
@@ -89,7 +90,7 @@ const Register = () => {
         />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   );
