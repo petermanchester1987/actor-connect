0d9cd41a159f3e4943ceb64fad7e9828cd77874c
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-09-25 18:32:48 +0100

    removed linter problems

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,10 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to="/profiles">Artistes</Link>
+      </li>
+
+      <li>
         <Link to="/dashboard">
           <i className="fas fa-user"></i>{" "}
           <span className="hide-sm">Dashboard</span>
@@ -25,7 +29,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const guestLinks = (
     <ul>
       <li>
-        <Link to="#!">Actors</Link>
+        <Link to="/profiles">Artistes</Link>
       </li>
       <li>
         <Link to="/register">Register</Link>
