96b915ca81e4e71858adb0d7cf1553746cd8d67d
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-09-28 13:38:49 +0100

    removed axios from posts actions file used api instead

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import api from "../utils/api";
 import { setAlert } from "./alert";
 import {
   ADD_POST,
@@ -15,7 +15,7 @@ import {
 
 export const getPosts = () => async (dispatch) => {
   try {
-    const res = await axios.get("/api/posts");
+    const res = await api.get("/posts");
 
     dispatch({
       type: GET_POSTS,
@@ -33,7 +33,7 @@ export const getPosts = () => async (dispatch) => {
 
 export const addLike = (id) => async (dispatch) => {
   try {
-    const res = await axios.put(`/api/posts/like/${id}`);
+    const res = await api.put(`/posts/like/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
@@ -51,7 +51,7 @@ export const addLike = (id) => async (dispatch) => {
 
 export const removeLike = (id) => async (dispatch) => {
   try {
-    const res = await axios.put(`/api/posts/unlike/${id}`);
+    const res = await api.put(`/posts/unlike/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
@@ -69,7 +69,7 @@ export const removeLike = (id) => async (dispatch) => {
 
 export const deletePost = (id) => async (dispatch) => {
   try {
-    await axios.delete(`/api/posts/${id}`);
+    await api.delete(`/posts/${id}`);
 
     dispatch({
       type: DELETE_POST,
@@ -88,14 +88,8 @@ export const deletePost = (id) => async (dispatch) => {
 //Add Post
 
 export const addPost = (formData) => async (dispatch) => {
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-
   try {
-    const res = await axios.post(`/api/posts`, formData, config);
+    const res = await api.post(`/posts`, formData);
 
     dispatch({
       type: ADD_POST,
@@ -115,7 +109,7 @@ export const addPost = (formData) => async (dispatch) => {
 
 export const getPost = (id) => async (dispatch) => {
   try {
-    const res = await axios.get(`/api/posts/${id}`);
+    const res = await api.get(`/posts/${id}`);
 
     dispatch({
       type: GET_POST,
@@ -132,18 +126,8 @@ export const getPost = (id) => async (dispatch) => {
 //Add Comment
 
 export const addComment = (postId, formData) => async (dispatch) => {
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-
   try {
-    const res = await axios.post(
-      `/api/posts/comment/${postId}`,
-      formData,
-      config
-    );
+    const res = await api.post(`/posts/comment/${postId}`, formData);
 
     dispatch({
       type: ADD_COMMENT,
@@ -163,7 +147,7 @@ export const addComment = (postId, formData) => async (dispatch) => {
 
 export const deleteComment = (postId, commentId) => async (dispatch) => {
   try {
-    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+    await api.delete(`/posts/comment/${postId}/${commentId}`);
 
     dispatch({
       type: REMOVE_COMMENT,
